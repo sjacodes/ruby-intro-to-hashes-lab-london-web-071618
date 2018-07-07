a11e268893e6ec5af4dffd891b7309ce85bad67c
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:29:16 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,30 +4,15 @@ end
 
 def actor
   actor = {
-    :name => "Dwayne The Rock Johnson"
+   :name =>"Dwayne The Rock Johnson"
   }
 end
 
 def monopoly
 	monopoly = {
-	  :railroads => {} 
-
-	  :railroads => {
-	    :pieces => 4,
-	    :names => {
-	      :reading_railroad => {},
-	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => {},
-	      :shortline => {}
-	    },
-	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
-	    }
-	  }
+	  :railroads => {}
 	}
+
 end
 
 def monopoly_with_second_tier
