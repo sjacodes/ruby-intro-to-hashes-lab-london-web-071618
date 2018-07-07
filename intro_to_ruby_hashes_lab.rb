8c0edf2a43d86de3bb20b5336691ac734cf5df46
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:24:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ end
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
-      :pieces => '4',
+      :pieces => 4,
       :names => {},
       :rent_in_dollars =>{}
       } 
@@ -28,7 +28,7 @@ end
 def monopoly_with_third_tier
   monopoly = {
     :railroads => {
-      :pieces => '4',
+      :pieces => 4,
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
