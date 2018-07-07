aeb7b9a554afc745d0163094607f38d1537964ea
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:26:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,10 +11,23 @@ end
 def monopoly
 	monopoly = {
 	  :railroads => {} 
+
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline => {}
+	    },
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
+	    }
+	  }
 	}
-end
-
-
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
@@ -27,21 +40,21 @@ end
 
 def monopoly_with_third_tier
   monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline => {}
-      },
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200.
-      }
-    } 
-  }
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {
+	      :reading_railroad => {},
+	      :pennsylvania_railroad => {},
+	      :b_and_o_railroad => {},
+	      :shortline => {}
+	    },
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25,
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
+	    }
+	  }
+	}
 end
 
