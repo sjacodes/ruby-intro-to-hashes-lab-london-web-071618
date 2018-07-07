da17879ee949ba71beb950813235b35951d90d82
Sarah Jacob <smjacob91@gmail.com>
2018-07-07 11:27:54 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,17 +31,19 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :names => {},
-      :rent_in_dollars =>{}
-    } 
-  }
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	monopoly = {
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {},
+	    :rent_in_dollars => {}
+	  }
+	}
 end
 
 def monopoly_with_third_tier
-  monopoly = {
+	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+		{
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
@@ -59,4 +61,3 @@ def monopoly_with_third_tier
 	  }
 	}
 end
-
